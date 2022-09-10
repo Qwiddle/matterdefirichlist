8abc73eb8866fde15b69df5032e107753b8f9ec0
Andrew Rubenstein <andjruben@gmail.com>
2022-09-10 10:25:44 -0400

    Pass accounts prop to CollapsibleTable

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     <div className="app">
       <SearchAppBar />
       <div className="content">
-        <CollapsibleTable />
+        <CollapsibleTable accounts={accounts} />
       </div>
     </div>
   )
