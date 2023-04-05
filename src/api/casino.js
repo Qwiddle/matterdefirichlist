b76fe87dd19be814026faf0b2649770ae6850cb5
Andrew Rubenstein <andjruben@gmail.com>
2023-04-05 00:04:05 -0400

    feat: update fetch limit to 10k

--- src/api/casino.js
+++ src/api/casino.js
@@ -1,6 +1,6 @@
 import { SALSA_CASINO_API } from './api';
 
-export const fetchUserBets = async (userAddress, limit = 1000) => {
+export const fetchUserBets = async (userAddress, limit = 10000) => {
   const req = `${SALSA_CASINO_API}/getUserBets?userAddress=${userAddress}&limit=${limit}`;
   const res = await (await fetch(req)).json();
 
@@ -9,7 +9,7 @@ export const fetchUserBets = async (userAddress, limit = 1000) => {
   return bet;
 }
 
-export const fetchUserInvestments = async (userAddress, limit = 1000) => {
+export const fetchUserInvestments = async (userAddress, limit = 10000) => {
   const req = `${SALSA_CASINO_API}/getUserInvestments?userAddress=${userAddress}&limit=${limit}`;
   const res = await (await fetch(req)).json();
 
